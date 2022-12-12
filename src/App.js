f0bd0629ed80076b985ca7c101d39ef6dd4c58bb
Josue.S.G <sebastian.gilm30@gmail.com>
2022-12-11 20:13:10 -0600

    Customer

--- src/App.js
+++ src/App.js
@@ -1,45 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
 
 function App() {
-  const baseUrl='https://localhost:44349/api/gestores';
+ const baseUrl='https://localhost:44306/api/customer';
+  //const baseUrl='https://localhost:44306/swagger/index.html';
   const [data, setData] = useState([])
-  const [modalEditar, setModalEditar] = useState(false)
-  const [modalInsertar, setModalInsertar] = useState(false)
-  const [modalEliminar, setModalEliminar] = useState(false)
-  const [gestorSeleccionado, setGestorSeleccionado] = useState({
-        id: '',
-        nombre: '',
-        lanzamiento: '',
-        desarrollador: ''
+  const [modalEdit, setModalEdit] = useState(false)
+  const [modalInsert, setModalInsert] = useState(false)
+  const [modalDelete, setModalDelete] = useState(false)
+  const [selectedCustomer, setSelectedCustomer] = useState({
+        idCustomer: '',
+        customerName: '',
+        companyName: '',
+        phoneNumber: ''
   })
 
+  //guardamo lo que el usuario escribe en los inputs
   const handleChange=e=> {
-    const {name, value}=e.target
-    setGestorSeleccionado({
-      ...gestorSeleccionado,
+    const {name, value}=e.target;
+    setSelectedCustomer({
+      ...selectedCustomer,
       [name]: value
     });
-      console.log(gestorSeleccionado)
+      console.log(selectedCustomer)
   }
 
-  const abrirCerrarModalInsertar = () => {
-    setModalInsertar(!modalInsertar)
+  const openColseModalInsert = () => {
+    setModalInsert(!modalInsert)
   }
 
-  const abrirCerrarModalEditar = () => {
-    setModalEditar(!modalEditar)
+  const openColseModalEdit = () => {
+    setModalEdit(!modalEdit)
   }
   
-  const abrirCerrarModalEliminar = () => {
-    setModalEliminar(!modalEliminar)
+  const openColseModalDelete = () => {
+    setModalDelete(!modalDelete)
   }
   
-  const peticionGet = async () => {
+  const requestGet = async () => {
     await axios.get(baseUrl)
     .then(response => {
       setData(response.data)
@@ -48,153 +49,153 @@ function App() {
     })
   }
 
-  const peticionPost = async () => {
-    delete gestorSeleccionado.id
-    gestorSeleccionado.lanzamiento = parseInt(gestorSeleccionado.lanzamiento)
-    await axios.post(baseUrl, gestorSeleccionado)
-    .then( response => {
+  const requestPost = async () => {
+    delete selectedCustomer.idCustomer
+    selectedCustomer.phoneNumber = parseInt(selectedCustomer.phoneNumber)
+    await axios.post(baseUrl, selectedCustomer)
+    .then(response => {
       setData(data.concat(response.data))
-      abrirCerrarModalInsertar()
+      openColseModalInsert()
     }).catch(error => {
       console.log(error)
     })
   } 
   
-  const peticionPut = async () => {
-    gestorSeleccionado.lanzamiento = parseInt(gestorSeleccionado.lanzamiento)
-    await axios.put(baseUrl+"/"+gestorSeleccionado.id, gestorSeleccionado)
-    .then( response => {
-      var respuesta = response.data
-      var dataAuxiliar = data
-      dataAuxiliar.map(gestor => {
-        if(gestor.id === gestorSeleccionado.id){
-          gestor.nombre = respuesta.nombre
-          gestor.lanzamiento = respuesta.lanzamiento
-          gestor.desarrollador = respuesta.desarrollador
+  const requestPut = async()=>{
+   selectedCustomer.phoneNumber=parseInt(selectedCustomer.phoneNumber)
+    await axios.put(baseUrl+"/"+selectedCustomer.idCustomer, selectedCustomer)
+    .then(response=>{
+      var respuesta=response.data
+      var dataAuxiliar=data
+      dataAuxiliar.map(customer=>{
+        if(customer.idCustomer===selectedCustomer.idCustomer){
+          customer.customerName=respuesta.customerName;
+          customer.companyName=respuesta.companyName;
+          customer.phoneNumber=respuesta.phoneNumber;
         }
-      })
-        abrirCerrarModalEditar()
-    }).catch(error => {
+      });
+      openColseModalEdit()
+    }).catch(error=>{
       console.log(error)
     })
   } 
 
-  const peticionDelete = async() => {
-    await axios.delete(baseUrl+"/"+gestorSeleccionado.id)
+  const requestDelete = async() => {
+    await axios.delete(baseUrl+"/"+selectedCustomer.idCustomer)
     .then(response => {
-      setData(data.filter(gestor => gestor.id!==response.data))
-      abrirCerrarModalEliminar()
+      setData(data.filter(customer => customer.idCustomer!==response.data))
+      openColseModalDelete()
     }).catch(error => {
       console.log(error)
     })
   }
 
-  const seleccionarGestor=(gestor, caso) => {
-    setGestorSeleccionado(gestor)
-    (caso==='Editar')?
-    abrirCerrarModalEditar(): abrirCerrarModalEliminar()
+  const selectModal=(customer, action)=>{
+    setSelectedCustomer(customer)
+    (action==='Edit')?
+    openColseModalEdit(): openColseModalDelete()
   }
 
-  useEffect(() => {
-    peticionGet()
+  useEffect(()=>{
+    requestGet()
   },[])
 
   return (
     <div className='App'>
-      <br></br>
+      <br/><br/>
+      <button onClick={()=>openColseModalInsert()} className="btn btn-success"> Insert new customer </button>
+      <br/><br/>
       <table className='table table-bordered'>
         <thead>
           <tr>
-            <th>Id</th>
-            <th>Nombre</th>
-            <th>Lanzamiento</th>
-            <th>Desarrollador</th>
-            <th>Acciones</th>
+            <th>Id Customer</th>
+            <th>Customer Name</th>
+            <th>Company Name</th>
+            <th>Phone Number</th>
+            <th>Acctions</th>
           </tr>
         </thead>
         <tbody>
-          {data.map(gestor=>(
-            <tr key={gestor.id}>
-              <td>{gestor.id}</td>
-              <td>{gestor.nombre}</td>
-              <td>{gestor.lanzamiento}</td>
-              <td>{gestor.desarrollador}</td>
+          {data.map(customer=>(
+            <tr key={customer.idCustomer}>
+              <td>{customer.idCustomer}</td>
+              <td>{customer.customerName}</td>
+              <td>{customer.companyName}</td>
+              <td>{customer.phoneNumber}</td>
               <td>
-                <button onClick={()=> abrirCerrarModalInsertar()} className='btn btn-outline-success'>Insertar Gestor</button>
-                <button onClick={()=> seleccionarGestor(gestor, 'Editar')} className='btn btn-outline-primary'>Editar</button> {" "}
-                <button onClick={()=> seleccionarGestor(gestor, 'Eliminar')} className='btn btn-outline-danger' >Eliminar</button>
+                <button onClick={()=>selectModal(customer, 'Edit')} className='btn btn-outline-primary'>Edit</button> {" "}
+                <button onClick={()=>selectModal(customer, 'Delete')} className='btn btn-outline-danger'>Delete</button>
               </td>
             </tr>
           ))}
-
         </tbody>
+
       </table>
-      
-      <Modal isOpen = {modalInsertar}>
-            <ModalHeader>Insertar Gestor de Base de Datos</ModalHeader>
+
+
+      <Modal isOpen = {modalInsert}>
+            <ModalHeader>Insertar Cliente a Base de Datos</ModalHeader>
             <ModalBody>
                 <div className='form-group'>
-                  <label>Nombre: </label>
+                  <label>Name: </label>
                   <br/>
-                  <input type='text' className='form-control' name='nombre' onChange={handleChange}/>
+                  <input type='text' className='form-control' name='customerName' onChange={handleChange}/>
                   <br/>
-                  <label>Lanzamiento: </label>
+                  <label>Company: </label>
                   <br/>
-                  <input type='text' className='form-control' name='lanzamiento' onChange={handleChange}/>
+                  <input type='text' className='form-control' name='companyName' onChange={handleChange}/>
                   <br/>
-                  <label>Desarollador: </label>
+                  <label>Phone Number: </label>
                   <br/>
-                  <input type='text' className='form-control' name='desarrollador' onChange={handleChange}/>
+                  <input type='text' className='form-control' name='phoneNumber' onChange={handleChange}/>
                   <br/>
                 </div>
             </ModalBody>
             <ModalFooter>
-            <button className='btn btn-primary' onClick={()=> peticionPost()}>Insertar</button> {" "}
-            <button className='btn btn-danger' onClick={()=> abrirCerrarModalInsertar()}>Cancelar</button>
+            <button className='btn btn-primary' onClick={()=> requestPost()}>Insert</button> {" "}
+            <button className='btn btn-danger' onClick={()=> openColseModalInsert()}>Cancel</button>
             </ModalFooter>
       </Modal>
 
-      <Modal isOpen = {modalEditar}>
-            <ModalHeader>Editar Gestor de Base de Datos</ModalHeader>
+      <Modal isOpen = {modalEdit}>
+            <ModalHeader>Editar Cliente de Base de Datos</ModalHeader>
             <ModalBody>
                 <div className='form-group'>
                   <label>Id:</label>
                   <br/>
-                  <input type='text' className='form-control' readOnly value ={gestorSeleccionado && gestorSeleccionado.id}/>
+                  <input type='text' className='form-control' readOnly value ={selectedCustomer && selectedCustomer.idCustomer}/>
                   <br/>
-                  <label>Nombre:</label>
+                  <label>Name:</label>
                   <br/>
-                  <input type='text' className='form-control' name='nombre' onChange={handleChange}value ={gestorSeleccionado && gestorSeleccionado.nombre}/>
+                  <input type='text' className='form-control' name='customerName' onChange={handleChange} value = {selectedCustomer && selectedCustomer.customerName}/>
                   <br/>
-                  <label>Lanzamiento:</label>
+                  <label>Company:</label>
                   <br/>
-                  <input type='text' className='form-control' name='lanzamiento' onChange={handleChange}value ={gestorSeleccionado && gestorSeleccionado.lanzamiento}/>
+                  <input type='text' className='form-control' name='companyName' onChange={handleChange} value = {selectedCustomer && selectedCustomer.companyName}/>
                   <br/>
-                  <label>Desarrollador:</label>
+                  <label>Phone:</label>
                   <br/>
-                  <input type='text' className='form-control' name='desarrollador' onChange={handleChange}value ={gestorSeleccionado && gestorSeleccionado.desarrollador}/>
+                  <input type='text' className='form-control' name='phoneNumber' onChange={handleChange} value = {selectedCustomer && selectedCustomer.phoneNumber}/>
                   <br/>
                 </div>
             </ModalBody>
             <ModalFooter>
-            <button className='btn btn-primary' onClick={() => peticionPut()}>Editar</button> {' '}
-            <button className='btn btn-danger' onClick={() => abrirCerrarModalEditar()}>Cancelar</button>
+            <button className='btn btn-primary' onClick={() => requestPut()}>Editar</button> {' '}
+            <button className='btn btn-danger' onClick={() => openColseModalEdit()}>Cancelar</button>
             </ModalFooter>
       </Modal>
 
-      <Modal  isOpen = {modalEliminar}>
+      <Modal isOpen = {modalDelete}>
             <ModalBody>
-              ¿Seguro que desea eliminar el registro {gestorSeleccionado && gestorSeleccionado.nombre}?
+              ¿Seguro que desea eliminar el registro {selectedCustomer && selectedCustomer.customerName}?
             </ModalBody>
             <ModalFooter>
-            <button className='btn btn-danger' onClick={() => peticionDelete()}>Si</button>
-            <button className='btn btn-secondary' onClick={() => abrirCerrarModalEliminar()}>No</button>
+            <button className='btn btn-danger' onClick={() => requestDelete()}>Si</button>
+            <button className='btn btn-secondary' onClick={() => openColseModalDelete()}>No</button>
             </ModalFooter>
       </Modal>
     </div>
-  )
-  
-
+  );
 }
 
 export default App;
